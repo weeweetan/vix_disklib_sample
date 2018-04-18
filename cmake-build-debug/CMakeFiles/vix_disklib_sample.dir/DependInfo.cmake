2db43bd771607632c317b3833747693a914aa17c
tanwei <1113513062@qq.com>
2018-04-18 19:49:02 +0800

    建立clion项目

---
+++ cmake-build-debug/CMakeFiles/vix_disklib_sample.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/d/Code/Project-C/vix_disklib_sample/vixDiskLibSample.cpp" "/cygdrive/d/Code/Project-C/vix_disklib_sample/cmake-build-debug/CMakeFiles/vix_disklib_sample.dir/vixDiskLibSample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/cygdrive/d/Code/Project-C/vmware-vix-disklib-distrib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/vix_disklib_sample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vix_disklib_sample.dir/vixDiskLibSample.cpp.o"
+  "vix_disklib_sample.pdb"
+  "vix_disklib_sample.exe"
+  "libvix_disklib_sample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vix_disklib_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
